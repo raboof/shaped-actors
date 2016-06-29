c1277b63ce8b51cb99e7975e7350eaaa70b03b9c
Arnout Engelen <arnouten@bzzt.net>
2016-06-29 19:48:15 +0200

    Rename 'performer' to 'witness' to better describe its purpose

--- src/main/scala/akkashaped/ShapedRef.scala
+++ src/main/scala/akkashaped/ShapedRef.scala
@@ -14,14 +14,14 @@ import akka.util.Timeout
 class ShapedRef[L <: HList](ref: ActorRef) {
   import ShapedRef._
 
-  def ask[I, O](in: I)(implicit timeout: Timeout, sender: ActorRef, performer: Performer[L, I, O]): O = {
+  def ask[I, O](in: I)(implicit timeout: Timeout, sender: ActorRef, witness: ShapeWitness[L, I, O]): O = {
     val askable: AskableActorRef = ref;
     askable.ask(in).asInstanceOf[O]
   }
 
-  def tell[I](in: I)(implicit sender: ActorRef, performer: Performer[L, I, Unit]): Unit = ref ! in
+  def tell[I](in: I)(implicit sender: ActorRef, witness: ShapeWitness[L, I, Unit]): Unit = ref ! in
 
-  def retryAfter[I, O](in: I, future: Future[_])(implicit timeout: Timeout, sender: ActorRef, performer: Performer[L, I, O], ec: ExecutionContext): O = {
+  def retryAfter[I, O](in: I, future: Future[_])(implicit timeout: Timeout, sender: ActorRef, witness: ShapeWitness[L, I, O], ec: ExecutionContext): O = {
     future.flatMap { value =>
       ref ! value
       val askable: AskableActorRef = ref;
@@ -39,20 +39,20 @@ object ShapedRef {
   /** Wrap an existing shaped actor */
   def wrap[T <: HList](actor: Actor with Shaped[T]): ShapedRef[T] = new ShapedRef(actor.self)
 
-  @implicitNotFound("Implicit not found: Performer[${L}, ${I}, ${O}]. You requested an element of type ${I} => ${O}, but there is none in the HList ${L}.")
-  trait Performer[L <: HList, I, O] extends DepFn2[L, I] with Serializable {
+  @implicitNotFound("No function of type ''${I} => ${O}'' found in shape ${L}.")
+  trait ShapeWitness[L <: HList, I, O] extends DepFn2[L, I] with Serializable {
     type In = I
     type Out = O
   }
 
-  object Performer {
-    implicit def perform[H1, H2, T <: HList]: Performer[(H1 => H2) :: T, H1, H2] =
-      new Performer[(H1 => H2) :: T, H1, H2] {
+  object ShapeWitness {
+    implicit def witness[H1, H2, T <: HList]: ShapeWitness[(H1 => H2) :: T, H1, H2] =
+      new ShapeWitness[(H1 => H2) :: T, H1, H2] {
         def apply(l: (H1 => H2) :: T, i: H1) = l.head(i)
       }
 
-    implicit def recurse[H, T <: HList, I, O](implicit p: Performer[T, I, O]): Performer[H :: T, I, O] =
-      new Performer[H :: T, I, O] {
+    implicit def recurse[H, T <: HList, I, O](implicit p: ShapeWitness[T, I, O]): ShapeWitness[H :: T, I, O] =
+      new ShapeWitness[H :: T, I, O] {
         def apply(l: H :: T, i: I) = p(l.tail, i)
       }
   }
